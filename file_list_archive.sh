a5162cfe23e1d929adb8f82e98d7123dc816fe24
Jonathan Hornung <jhornung@exegy.com>
2020-10-19 13:58:56 -0500

    Rough first version work

--- file_list_archive.sh
+++ file_list_archive.sh
@@ -5,5 +5,12 @@
 # file_list_archive
 # Version 0.1-1000
 
-echo "Hi"
+echo "Saving output"
 
+for folder in $(cat list_of_folders.txt)
+do
+    ls -lhaR $folder > $folder_ls_output.txt
+    #tree /F /A $folder > $folder_tree_output.txt
+done
+
+# Add tar compression here and cleanup files
